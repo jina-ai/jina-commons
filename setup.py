828d54992a37096e959f05375d0f78521a90b45c
Maximilian Werk <maximilian.werk@jina.ai>
2021-06-07 13:25:24 +0200

    feat: first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages
+import setuptools
+
+setuptools.setup(
+    name="jina-commons",
+    version="0.0.1",
+    author='Jina Dev Team',
+    author_email='dev-team@jina.ai',
+    description="Shared functions for Jina Executors",
+    url="https://github.com/jina-ai/jina-commons",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(where='.', include='jina.*'),
+    python_requires=">=3.7",
+)
