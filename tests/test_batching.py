c411c5e1dcee9bd5683087623ab06ebab2af7c3c
Tadej Svetina <tadej.svetina@jina.ai>
2021-07-22 16:21:28 +0200

    fix: fix filtering by text attr in batch generator

--- tests/test_batching.py
+++ tests/test_batching.py
@@ -12,9 +12,7 @@ NUM_DOCS = 15
 
 @pytest.fixture()
 def doc_array() -> DocumentArray:
-    return DocumentArray([
-        Document(text='test') for _ in range(NUM_DOCS)
-    ])
+    return DocumentArray([Document(text='test') for _ in range(NUM_DOCS)])
 
 
 @pytest.fixture()
@@ -24,18 +22,12 @@ def docs_root(doc_array: DocumentArray) -> DocumentArray:
 
 @pytest.fixture()
 def docs_chunk(doc_array: DocumentArray) -> DocumentArray:
-    return DocumentArray([
-        Document(chunks=doc_array)
-    ])
+    return DocumentArray([Document(chunks=doc_array)])
 
 
 @pytest.fixture()
 def docs_chunk_chunk(doc_array) -> DocumentArray:
-    return DocumentArray([
-        Document(chunks=[
-            Document(chunks=doc_array)
-        ])
-    ])
+    return DocumentArray([Document(chunks=[Document(chunks=doc_array)])])
 
 
 @pytest.mark.parametrize(
@@ -46,46 +38,56 @@ def docs_chunk_chunk(doc_array) -> DocumentArray:
         (lazy_fixture('docs_root'), 5, 'text', [5, 5, 5], ['r']),
         (lazy_fixture('docs_root'), 5, 'text', [-1], ['c']),
         (lazy_fixture('docs_root'), 5, 'blob', [-1], ['r']),
-
         (lazy_fixture('docs_chunk'), 10, 'text', [10, 5], ['c']),
         (lazy_fixture('docs_chunk'), 20, 'text', [15], ['c']),
         (lazy_fixture('docs_chunk'), 5, 'text', [5, 5, 5], ['c']),
         (lazy_fixture('docs_chunk'), 5, 'text', [1], ['r']),
         (lazy_fixture('docs_chunk'), 5, 'blob', [-1], ['c']),
-
         (lazy_fixture('docs_chunk_chunk'), 10, 'text', [10, 5], ['cc']),
         (lazy_fixture('docs_chunk_chunk'), 20, 'text', [15], ['cc']),
         (lazy_fixture('docs_chunk_chunk'), 5, 'text', [5, 5, 5], ['cc']),
         (lazy_fixture('docs_chunk_chunk'), 5, 'text', [1], ['c']),
         (lazy_fixture('docs_chunk_chunk'), 5, 'blob', [-1], ['cc']),
-    ]
-
+    ],
 )
 def test_batching(
     docs: DocumentArray,
     batch_size: int,
     filter_attr: str,
     expected_sizes: List[int],
-    traversal_path: List[str]
+    traversal_path: List[str],
 ):
     generator = get_docs_batch_generator(
         docs,
         traversal_path=traversal_path,
         batch_size=batch_size,
-        needs_attr=filter_attr
+        needs_attr=filter_attr,
     )
     for batch, expected_size in zip(generator, expected_sizes):
-        assert len(batch) == expected_size, f'Expected size {expected_size} but got {len(batch)}'
+        assert (
+            len(batch) == expected_size
+        ), f'Expected size {expected_size} but got {len(batch)}'
 
 
 def test_docs_array_none():
     docs = None
-    generator = get_docs_batch_generator(
-        docs,
-        traversal_path=['r'],
-        batch_size=1
-    )
+    generator = get_docs_batch_generator(docs, traversal_path=['r'], batch_size=1)
     count = 0
     for count, item in enumerate(generator):
         pass
     assert count == 0
+
+
+def test_needs_text_empty():
+    """
+    Test the case where needs_attr='text', and empty documents are part of the array
+    """
+
+    docs = DocumentArray([Document(), Document(text='text')])
+    generator = get_docs_batch_generator(
+        docs, traversal_path=['r'], batch_size=1, needs_attr='text'
+    )
+    filtered_docs = list(generator)
+
+    assert len(filtered_docs) == 1 and len(filtered_docs[0]) == 1
+    assert filtered_docs[0][0].text == 'text'
